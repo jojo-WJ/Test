96a84c514897fd1d92c3d31b4d3a0c3610c8cb92
jojo-WJ <1059114801@qq.com>
2017-03-10 14:19:24 +0800

    123
    Signed-off-by: jojo-WJ <1059114801@qq.com>

---
+++ https/MakeItame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+public class MakeItame : MonoBehaviour {
+
+    [MenuItem("MyMenu/AtlasMaker")]
+    static private void MakeAtlas()
+    {
+        string spriteDir = Application.dataPath + "/Resources/Sprite";
+
+        if (!Directory.Exists(spriteDir))
+        {
+            Directory.CreateDirectory(spriteDir);
+        }
+
+        DirectoryInfo rootDirInfo = new DirectoryInfo(Application.dataPath + "/Atlas");
+        foreach (DirectoryInfo dirInfo in rootDirInfo.GetDirectories())
+        {
+            foreach (FileInfo pngFile in dirInfo.GetFiles("*.png", SearchOption.AllDirectories))
+            {
+                string allPath = pngFile.FullName;
+                //Debug.Log(allPath);//D:\My unity project\2017\TextrueTest\Assets\Atlas\yaopin\UI_Item_Crafting_Cup_SourCream.tex.png
+                string assetPath = allPath.Substring(allPath.IndexOf("Assets"));
+
+                //Debug.Log(assetPath); //Assets\Atlas\yaopin\UI_Item_Crafting_Cup_SourCream.tex.png
+
+
+                Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+                GameObject go = new GameObject(sprite.name);
+                go.AddComponent<SpriteRenderer>().sprite = sprite;
+                allPath = spriteDir + "/" + sprite.name + ".prefab";
+                string prefabPath = allPath.Substring(allPath.IndexOf("Assets"));
+                PrefabUtility.CreatePrefab(prefabPath, go);
+                GameObject.DestroyImmediate(go);
+            }
+            foreach (FileInfo pngFile in dirInfo.GetFiles("*.jpg", SearchOption.AllDirectories))
+            {
+                string allPath = pngFile.FullName;
+                string assetPath = allPath.Substring(allPath.IndexOf("Assets"));
+                Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+                GameObject go = new GameObject(sprite.name);
+                go.AddComponent<SpriteRenderer>().sprite = sprite;
+                allPath = spriteDir + "/" + sprite.name + ".prefab";
+                string prefabPath = allPath.Substring(allPath.IndexOf("Assets"));
+                PrefabUtility.CreatePrefab(prefabPath, go);
+                GameObject.DestroyImmediate(go);
+            }
+        }
+    }
+}
